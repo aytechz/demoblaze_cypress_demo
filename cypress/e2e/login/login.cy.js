437d72d2eec3ba9ee4b9e03e2524229c2ff8803e
aytechz <fmaytekin@gmail.com>
2023-09-13 21:01:18 -0700

    Login page obj and login method

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -3,9 +3,6 @@ import loginPage from "../../support/page/login.page";
 describe('Login Tests', ()=>{
     it('Visit', () => {
         cy.visit('https://www.saucedemo.com');
-
-        loginPage.usernameField().type(Cypress.env('locked_u'));
-        cy.get('#password').type(Cypress.env('pass'), {log: false});
-        cy.get('#login-button').click();
+        loginPage.login('standard_u')
     });
 })
--- cypress/support/page/login.page.js
+++ cypress/support/page/login.page.js
@@ -1,5 +1,16 @@
 class LoginPage{
     usernameField() {return cy.get('#user-name')}
+    passwordField() {return cy.get('#password')}
+    loginButton() {return cy.get('#login-button')}
+
+    /**Login method use 
+     * the usernames from cypress.config.js file 
+     * 'standard_u'*/
+    login(user){
+        this.usernameField().type(Cypress.env(user))
+        this.passwordField().type(Cypress.env('pass'))
+        this.loginButton().click()
+    }
 
 
 }
